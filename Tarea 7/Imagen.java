cf051ac122953733c7f1899b3ddb03e9f194e386
McFlyZero <a217209792@unison.mx>
2023-02-14 22:58:44 -0700

    Tarea 7
    Signed-off-by: McFlyZero <a217209792@unison.mx>

---
+++ Tarea 7/Imagen.java
@@ -0,0 +1,205 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Imagen extends JLabel implements Runnable, KeyListener {
+    ImageIcon icon;
+    String url1, url2;
+    int speed;
+    int posY;
+    boolean flag = true;
+    boolean flag2 = true;
+    int x = speed;
+    int limit = 250;
+    private boolean runStatus = false, right = false, left = false, shift = false, up = false;
+
+    public Imagen(String url1, String url2, int speed, int posY) {
+        this.url1 = url1;
+        this.url2 = url2;
+        this.speed = speed;
+        this.posY = posY;
+
+        icon = new ImageIcon(this.getClass().getResource(url1));
+        setIcon(icon);
+    }
+
+    public void run() {
+        runStatus = true;
+        System.out.println("runStatus: " + runStatus);
+        while (runStatus) {
+            try {
+                Thread.sleep(10);
+                if (up) {
+                    // posY = 40;
+                    jump();
+                }
+                /*
+                 * else {
+                 * posY = 60;
+                 * }
+                 */
+
+                if (right) {
+
+                    if (x < limit) {
+
+                        if (shift) {
+                            x = x + 20;
+                        } else {
+                            x = x + 5;
+                        }
+
+                        if (flag) {
+                            icon = new ImageIcon(this.getClass().getResource(url1));
+                        } else {
+                            icon = new ImageIcon(this.getClass().getResource(url2));
+                        }
+
+                        flag = !flag;
+                        setIcon(icon);
+                        setBounds(x, posY, 42, 42);
+                    }
+                }
+
+                if (left) {
+                    if (x > 0) {
+                        if (shift) {
+                            x = x - 20;
+                        } else {
+                            x = x - 5;
+                        }
+
+                        if (flag2) {
+                            icon = new ImageIcon(this.getClass().getResource(url1));
+                        } else {
+                            icon = new ImageIcon(this.getClass().getResource(url2));
+                        }
+
+                        flag2 = !flag2;
+                        setIcon(icon);
+                        setBounds(x, posY, 42, 42);
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+        }
+    }
+
+    private void jump() {
+        try {
+            System.out.println("Entré a salto");
+            for (int i = 0; i < 20; i++) {
+                posY = posY - 1;
+                setBounds(x, posY, 42, 42);
+                Thread.sleep(5);
+            }
+            for (int i = 0; i < 20; i++) {
+                posY = posY + 1;
+                setBounds(x, posY, 42, 42);
+                Thread.sleep(2);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        System.out.println("Entré a keyTyped");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        if (runStatus) {
+            if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+                right = true;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+                left = true;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_SHIFT) {
+                shift = true;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_UP) {
+                up = true;
+            }
+        }
+
+        /*
+         * switch (ke.getKeyCode()) {
+         * 
+         * case KeyEvent.VK_RIGHT:
+         * System.out.println("Derecha");
+         * if (x < limit) {
+         * System.out.println("Shift: " + ke.isShiftDown());
+         * if (ke.isShiftDown()) {
+         * System.out.println("Shift presionado");
+         * x = x + 20;
+         * }else{
+         * x = x + 5;
+         * }
+         * 
+         * if (flag) {
+         * icon = new ImageIcon(this.getClass().getResource(url1));
+         * } else {
+         * icon = new ImageIcon(this.getClass().getResource(url2));
+         * }
+         * 
+         * flag = !flag;
+         * setIcon(icon);
+         * setBounds(x, posY, 42, 42);
+         * break;
+         * }
+         * 
+         * case KeyEvent.VK_LEFT:
+         * if (x > 0) {
+         * System.out.println("Izquierda");
+         * if (ke.isShiftDown()) {
+         * System.out.println("Shift presionado");
+         * x = x - 20;
+         * }else{
+         * x = x - 5;
+         * }
+         * 
+         * if (flag2) {
+         * icon = new ImageIcon(this.getClass().getResource(url1));
+         * } else {
+         * icon = new ImageIcon(this.getClass().getResource(url2));
+         * }
+         * 
+         * flag2 = !flag2;
+         * setIcon(icon);
+         * setBounds(x, posY, 42, 42);
+         * break;
+         * }
+         * 
+         * default:
+         * System.out.println("Otra tecla");
+         * break;
+         * 
+         * }
+         */
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        if (runStatus) {
+            if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+                right = false;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+                left = false;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_SHIFT) {
+                shift = false;
+            }
+            if (ke.getKeyCode() == KeyEvent.VK_UP) {
+                up = false;
+            }
+        }
+    }
+
+}
---
+++ Tarea 7/Ventana.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Ventana extends JFrame {
+    public Ventana() {
+        initValues();
+    }
+
+    private void initValues() {
+        JButton btnStart = new JButton("Start");
+        Imagen img1 = new Imagen("images/mario1.png", "images/mario2.png", 1, 60);
+        //Imagen img2 = new Imagen("/images/link1.png", "/images/link2.png", 1, 90);
+
+        btnStart.setBounds(10, 10, 75, 25);
+        img1.setBounds(0, 60, 42, 42);
+        //img2.setBounds(0, 90, 42, 42);
+        img1.setFocusable(true);
+       //img2.setFocusable(true);
+        
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Thread t1 = new Thread(img1);
+                t1.start();
+                // Thread t2 = new Thread(img2);
+                //  t2.start();
+                btnStart.setEnabled(false);
+                /*Thread t3 = new Thread() {
+                    public void run() {
+                        try {
+                            t1.join();
+                            btnStart.setEnabled(true);
+                        } catch (Exception ex) {
+                            System.out.println("Error: " + ex.getMessage());
+                        }
+                    }
+                };
+                t3.start();*/
+            }
+        };
+
+        add(btnStart);
+        add(img1);
+        //add(img2);
+
+        btnStart.addActionListener(listener);
+        img1.addKeyListener(img1);
+
+        setTitle("Tarea 5");
+        setSize(300, 300);
+        setResizable(false);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }// end initValues
+
+}
